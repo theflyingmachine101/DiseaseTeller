cd7ed02dc80293677f0e3bb4b1ea4b3432cd17c2
suryapts07 <surya.singh15071999@gmail.com>
2020-03-14 18:23:54 +0530

    Updated diseasesList.js and corrected some wrong entries in diseaseSymptoms.js

--- diseaseList.js
+++ diseaseList.js
@@ -5,7 +5,39 @@
      disease:["Common Cold","Influenza","Pneumonia"]
    },
    {
+     category:"Digestion",
+     disease:["Diarrhoea","Indigestion","Intestinal Gas","Constipation"]
+   },
+   {
+     category:"Communicable",
+     disease:["Malaria","Dengue"]
+   },
+   {
+     category:"Blood Pressure",
+     disease:["Hypertension"]
+   },
+   {
+     category:"Mental Health",
+     disease:["Stress","Panic Attacks"]
+   },
+   {
+     category:"Pediatrics (Kids)",
+     disease:["Chicken Pox","Measles","Mumps"]
+   },
+   {
      category:"Skin",
      disease:["Cellulitis","Dermatitis","Acne"]
+   },
+   {
+     category:"Sleep",
+     disease:["Insomnia","Sleep Apnea"]
+   },
+   {
+     category:"Thyroid",
+     disease:["Hyperthyroidism","Hypothyroidism"]
+   },
+   {
+     category:"Urinary",
+     disease:["Kidney Stones","Urinary Tract Infection"]
    }
 ];
