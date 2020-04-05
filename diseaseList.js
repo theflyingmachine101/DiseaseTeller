beea09d8e16cc52224c92e1c041df2567e25eaad
suryapts07 <surya.singh15071999@gmail.com>
2020-04-05 15:57:04 +0530

    front end work

--- diseaseList.js
+++ diseaseList.js
@@ -1,43 +1,43 @@
 //Disease Category List
- module.exports=[
-   {
-     category:"Cold and Flu",
-     disease:["Common Cold","Influenza","Pneumonia"]
-   },
-   {
-     category:"Digestion",
-     disease:["Diarrhoea","Indigestion","Intestinal Gas","Constipation"]
-   },
-   {
-     category:"Communicable",
-     disease:["Malaria","Dengue"]
-   },
-   {
-     category:"Blood Pressure",
-     disease:["Hypertension"]
-   },
-   {
-     category:"Mental Health",
-     disease:["Stress","Panic Attacks"]
-   },
-   {
-     category:"Pediatrics (Kids)",
-     disease:["Chicken Pox","Measles","Mumps"]
-   },
-   {
-     category:"Skin",
-     disease:["Cellulitis","Dermatitis","Acne"]
-   },
-   {
-     category:"Sleep",
-     disease:["Insomnia","Sleep Apnea"]
-   },
-   {
-     category:"Thyroid",
-     disease:["Hyperthyroidism","Hypothyroidism"]
-   },
-   {
-     category:"Urinary",
-     disease:["Kidney Stones","Urinary Tract Infection"]
-   }
+module.exports = [
+  {
+    category: "Cold and Flu",
+    disease: ["Common Cold", "Influenza", "Pneumonia"]
+  },
+  {
+    category: "Digestion",
+    disease: ["Diarrhoea", "Indigestion", "Intestinal Gas", "Constipation"]
+  },
+  {
+    category: "Communicable",
+    disease: ["Malaria", "Dengue"]
+  },
+  {
+    category: "Blood Pressure",
+    disease: ["Hypertension"]
+  },
+  {
+    category: "Mental Health",
+    disease: ["Stress", "Panic Attacks"]
+  },
+  {
+    category: "Pediatrics (Kids)",
+    disease: ["Chicken Pox", "Measles", "Mumps"]
+  },
+  {
+    category: "Skin",
+    disease: ["Cellulitis", "Dermatitis", "Acne"]
+  },
+  {
+    category: "Sleep",
+    disease: ["Insomnia", "Sleep Apnea"]
+  },
+  {
+    category: "Thyroid",
+    disease: ["Hyperthyroidism", "Hypothyroidism"]
+  },
+  {
+    category: "Urinary",
+    disease: ["Kidney Stones", "Urinary Tract Infection"]
+  }
 ];
--- homeRemedies.js
+++ homeRemedies.js
@@ -1,3 +1,64 @@
-module.exports={
-  "Influenza":["Do salt water gargle","Drink a lot of fluid","Take a warm bath", "Drink Ginger Tea"]
+module.exports = {
+  "Common Cold": ["Do salt water gargle", "Drink a lot of fluid", "Take a warm bath", "Drink Ginger Tea"],
+
+  "Diarrhoea": ["Drink plenty of fluids to avoid dehydration", "Avoid milk as it can make diarrhoea worst",
+    "Sport Beverages (eg. Gatorade or Powerade) can be beneficial", "Avoid greasy or fatty foods",
+    "Follow BRAT diet(ie. Bananas, Rice, Apple sauce, Toast)"],
+
+  "Indigestion": ["Drink a cup of peppermint tea or suck a peppermint candy after meals",
+    "Drink chamomile tea as it can ease gut discomfort and relieve indigestion",
+    "Drink apple cider vinegar by adding one to two teaspoons of it in a cup of water",
+    "Drink a cup of ginger tea as needed to soothe you stomach and get rid of indigestion",
+    "Put half teaspoon of crushed fennel seed in water and boil it for 10 minutes and drink",
+    "Drink baking soda solution", "Mix a tablespoon of lemon juice in hot or warm water and drink a few minutes before eating"],
+
+  "Intestinal Gas": ["Exercise can help release trapped gas and gas pain(try walking after meals to avoid gas)",
+    "Lactase supplements may help your body to digest lactose", "Peppermint tea may reduce symptoms of irritable bowel syndrome like gas",
+    "Drinking chamomile tea before meals and at bedtime may reduce symptoms",
+    "Activated charcoal also helps eliminate gas trapped in your colon(take tablets right before and one hour after meals)",
+    "Dilute a tablespoon of apple cider vinegar in water or tea and drink right before meals or upto three times daily as long as need to reduce symptoms"],
+
+  "Constipation": ["Drink more water", "Eat more fibre which are soluble and non-fermentable fibres as people who   are constipated are often told to increase their fibre intake because increasing fibre intake is thought to increase the bulk and consistency of bowel movements making them easier to pass",
+    "Exercise more as it seems to reduce some symptoms of constipation",
+    "Drink coffee(especially caffeinated coffee) as it can stimulate the muscles of intestine and  the urge to go to bathroom",
+    "Probiotics may also help prevent chronic constipation",
+    "Eat prebiotic fibres as they improve digestive health by feeding the friendly bacteria in your gut", "Avoid dairy products"],
+
+  "Malaria": ["Apple cider vinegar mixed with water and soaked in a towel and kept on calves for 10 to 12 minutes helps",
+    "Boil ginger in water and drink one to two cups on a daily basis",
+    "Boil cinnamon and pepper powder in water and add honey to the strained water and drink it one to two times a day",
+    "Consume two to three glasses of orange juice or grapefruit juice on a daily basis",
+    "Have two cups of herbal tea or turmeric milk everyday"],
+
+  "Dengue": ["Boil two small stems of Giloy plant in a glass of water and consume this water when it is little warm",
+    "You can also add few drops of giloy juice to a cup of boiled water and drink this twice a day",
+    "Papaya leaf juice is a great remedy to increase platelet count and it also improves immunity which helps in treating dengue",
+    "You can also drink fresh guava juice for relief from dengue",
+    "Fenugreek seeds are also rich in multiple nutrients which help in controlling dengue fever",
+    "Add immunity boosting foods to your diet like citrus foods(eg. garlic, almonds, turmeric and many more..)"],
+
+  "Hypertension": ["Walk and exercise regularly", "Reduce your sodium intake mainly salt",
+    "Avoid processed food as they contain large sodium", "Drink less alcohol as alcohol may elevate the blood pressure",
+    "Eat more potassium rich food(ie. leafy vegetables tomatoes, potatoes, sweet potatoes, melons, bananas, avocados, orangese, and apricots), Cutdown your caffeine(coffee) intake",
+    "Learn to manage stress as stress is the key driver of high blood pressure", "Eat dark chocolate or cocoa",
+    "If you are overweight then losing weight can make a big difference for your heart health", "Quit smoking",
+    "Reduce intake of added sugar and refined carbs"],
+
+  "Stress": ["Daily exercise will help you to reduce your stress", "Meditation is also a powerful way to reduce stress",
+    "Perform relaxation exercises", "Some research suggest that journaling and other form of writing can help people to cope better with anxiety",
+    "Try to make better strategies for your time management",
+    "Aroma Therapy ie. smelling soothing plants oils can help to ease stress and anxiety",
+    "Herbal teas also help with anxiety and ease sleep", "Spending time with animals can also help in reducing stress"],
+
+  "Panic Attacks": ["Regular exercise will help to reduce your stress", "If you smoke or consume alcohol then you should stop doing so",
+    "If you have chronic anxiety then caffeine is not your friend and you should stop consuming caffeine",
+    "Get some good sleep", "You should start meditation also as meditation is a very good way to reduce stress and panic attacks",
+    "Eat a healthy diet", "Practice deep breathing", "You may also try aroma therapy ie. smelling flower oils may help you",
+    "Drink chamomile tea as it will calm frayed nerves and promote sleep"],
+
+  "Chicken Pox": ["Apply calamine lotion as it contains zinc oxide which helps in reducing itching",
+    "If chickenpox has also appeared inside your mouth then encouraging to suck on sugar free popsicles can be a good way to soothe mouth sores", "Oatmeal baths can be soothing and itch relieving for chickenpox as taking a bath won’t spread the chickenpox from one are of your skin to another",
+    "Wear mittens to prevent scratching",
+    "Add one cup of baking soda to a shallow luke warm water and  bath for 15-20 minutes for two to three times a day",
+    "Use chamomile compresses", "Give approved pain relievers"]
 }
